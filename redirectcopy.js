3375a66ac0d564a82a2eae5af8faffdec4133d75
vothanhhung28121983 <65288561+vothanhhung28121983@users.noreply.github.com>
2020-05-15 18:46:01 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://thanhhunggpland.blogspot.com'>https://thanhhunggpland.blogspot.com"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://gp-land.vn/'>https://gp-land.vn</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
